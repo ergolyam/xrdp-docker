d00731bf9fa7c493695d88f9b309b51d9d8a789f
Grisha <grisha765@tuta.io>
2025-05-02 04:10:17 +0300

    [+] add xautolock starter

---
+++ rootfs/etc/profile.d/timeout.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ash
+
+TIMEFILE=/tmp/timeout
+
+[ -s "$TIMEFILE" ] || return
+
+[ -r "$TIMEFILE" ] && timeout=$(tr -d '[:space:]' < "$TIMEFILE")
+
+case "$timeout" in
+    ''|*[!0-9]*)
+        return
+        ;;
+    0|00)
+        return
+        ;;
+esac
+
+pgrep -u "$USER" -x xautolock >/dev/null &&
+    return
+
+xautolock -time "$timeout" -detectsleep -locker "icesh logout" &
