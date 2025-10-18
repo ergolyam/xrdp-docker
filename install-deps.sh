46176a2ac101b267417284ad2d66fab8141b4a92
ergolyam <grisha765@tuta.io>
2025-10-19 00:50:12 +0300

    [f] style

--- install-deps.sh
+++ install-deps.sh
@@ -5,27 +5,27 @@ TARGET="$(printf '%s' "$2" | tr '[:upper:]' '[:lower:]')"
 
 case "${DISTRO}_${TARGET}" in
     alpine_builder)
-        apk add --no-cache \
-          build-base \
-          git \
-          autoconf \
-          automake \
-          check-dev \
-          cmocka-dev \
-          libtool \
-          openssl-dev \
-          libx11-dev \
-          libxfixes-dev \
-          libxrandr-dev \
-          libjpeg-turbo-dev \
-          linux-headers \
-          nasm \
-          linux-pam-dev \
-          opus-dev \
-          libdrm-dev \
-          xorg-server-dev \
-          openh264-dev
-        ;;
+      apk add --no-cache \
+        build-base \
+        git \
+        autoconf \
+        automake \
+        check-dev \
+        cmocka-dev \
+        libtool \
+        openssl-dev \
+        libx11-dev \
+        libxfixes-dev \
+        libxrandr-dev \
+        libjpeg-turbo-dev \
+        linux-headers \
+        nasm \
+        linux-pam-dev \
+        opus-dev \
+        libdrm-dev \
+        xorg-server-dev \
+        openh264-dev
+      ;;
     alpine_main)
       apk add --no-cache \
         openssl \
